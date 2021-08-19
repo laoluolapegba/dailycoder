f4e08f3865527ae824366fab3823e4a00bc1ff57
Olaoluwa Olapegba <laoluolapegba@gmail.com>
2021-08-19 10:52:34 +0100

    add project tosolution ,add gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dailycoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
